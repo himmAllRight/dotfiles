db49529d72e3087df21024776a591f8300873894
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-02-15 19:30:15 -0500

    added arch install script

---
+++ archInstallScript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pacaur -S i3 dmenu
+pacaur -S weechat
+pacaur -S vim
+pacaur -S mplayer
+pacaur -S tty-clock
+pacaur -S dropbox
+pacaur -S nitrogen
+pacaur -S libreoffice
+pacaur -S archey
+pacaur -S scrot
+pacaur -S mpd
+pacaur -S pianobar
+pacaur -S ncmpcpp
+pacaur -S rsync
