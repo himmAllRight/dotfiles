c12336da821a4fc3df431d703f1d00910387a442
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-04-14 10:23:13 -0400

    added the working laptop config

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -38,7 +38,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init(awful.util.getdir("config") .. "/themes/powerarrow-darker/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/themes/holo/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
@@ -179,58 +179,6 @@ for s = 1, screen.count() do
 
     -- Create the wibox
     mywibox[s] = awful.wibox({ position = "top", screen = s })
-    
-    
-    -- Create an awesomeMpd widget
-    
-    local awesompd = require("awesompd/awesompd")
-  musicwidget = awesompd:create() -- Create awesompd widget
-  musicwidget.font = "Liberation Mono" -- Set widget font 
-  musicwidget.scrolling = true -- If true, the text in the widget will be scrolled
-  musicwidget.output_size = 30 -- Set the size of widget in symbols
-  musicwidget.update_interval = 10 -- Set the update interval in seconds
-  -- Set the folder where icons are located (change username to your login name)
-  musicwidget.path_to_icons = "/home/username/.config/awesome/awesompd/icons" 
-  -- Set the default music format for Jamendo streams. You can change
-  -- this option on the fly in awesompd itself.
-  -- possible formats: awesompd.FORMAT_MP3, awesompd.FORMAT_OGG
-  musicwidget.jamendo_format = awesompd.FORMAT_MP3
-  -- If true, song notifications for Jamendo tracks and local tracks will also contain
-  -- album cover image.
-  musicwidget.show_album_cover = true
-  -- Specify how big in pixels should an album cover be. Maximum value
-  -- is 100.
-  musicwidget.album_cover_size = 50
-  -- This option is necessary if you want the album covers to be shown
-  -- for your local tracks.
-  musicwidget.mpd_config = "/home/username/.mpdconf"
-  -- Specify the browser you use so awesompd can open links from
-  -- Jamendo in it.
-  musicwidget.browser = "firefox"
-  -- Specify decorators on the left and the right side of the
-  -- widget. Or just leave empty strings if you decorate the widget
-  -- from outside.
-  musicwidget.ldecorator = " "
-  musicwidget.rdecorator = " "
-  -- Set all the servers to work with (here can be any servers you use)
-  musicwidget.servers = {
-     { server = "127.0.0.1",
-          port = 6600 } }
-  -- Set the buttons of the widget
-  musicwidget:register_buttons({ { "", awesompd.MOUSE_LEFT, musicwidget:command_playpause() },
-      			         { "Control", awesompd.MOUSE_SCROLL_UP, musicwidget:command_prev_track() },
- 			         { "Control", awesompd.MOUSE_SCROLL_DOWN, musicwidget:command_next_track() },
- 			         { "", awesompd.MOUSE_SCROLL_UP, musicwidget:command_volume_up() },
- 			         { "", awesompd.MOUSE_SCROLL_DOWN, musicwidget:command_volume_down() },
- 			         { "", awesompd.MOUSE_RIGHT, musicwidget:command_show_menu() },
-                                 { "", "XF86AudioLowerVolume", musicwidget:command_volume_down() },
-                                 { "", "XF86AudioRaiseVolume", musicwidget:command_volume_up() },
-                                 { modkey, "Pause", musicwidget:command_playpause() } })
-  musicwidget:run() -- After all configuration is done, run the widget
-    
-    -- wifi widget
-
- 
 
     -- Widgets that are aligned to the left
     local left_layout = wibox.layout.fixed.horizontal()
@@ -241,10 +189,8 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
-    right_layout:add(musicwidget.widget)  -- mpd
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
-    
 
     -- Now bring it all together (with the tasklist in the middle)
     local layout = wibox.layout.align.horizontal()
@@ -493,10 +439,3 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
-
-
-
-
-
-
-
