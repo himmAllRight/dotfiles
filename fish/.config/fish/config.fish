57c4fd56ad9ab87ef8ec790ba5e724a92ee2d868
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2017-03-06 12:54:00 -0500

    Removed old links

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,3 @@
-# Connect
-alias desktop="ssh ryan@`echo ~/owncloud/current_ip.txt`"
-alias himmelwr="ssh -p 1729 ryan@`cat ~/owncloud/himmelwr.txt`"
-alias pidgey='mosh --ssh="ssh -p 1729" ryan@pidgey'
-alias umb="ssh rhimmelw@users.cs.umb.edu"
-
 # Radio
 alias jblive="mpv http://jblive.fm"
 alias jbliveV="mpv rtsp://jblive.videocdn.scaleengine.net/jb-live/play/jblive.stream"
