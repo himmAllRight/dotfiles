3181b485c3425c1200247be53cb5c79ba1da1a2d
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2017-05-09 05:18:04 -0400

    Updated locker

--- i3/.config/i3/i3lock/locker.sh
+++ i3/.config/i3/i3lock/locker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 scrot /tmp/screen_locked.png -c -d 1
-convert /tmp/screen_locked.png -scale 5% -scale 2000% /tmp/screen_locked2.png
+convert /tmp/screen_locked.png -scale 2.5% -scale 4000% /tmp/screen_locked2.png
 i3lock -f -i /tmp/screen_locked2.png
