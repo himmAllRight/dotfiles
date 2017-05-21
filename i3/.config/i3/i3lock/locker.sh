93044eac05c1986cc4b033e8d8d268e05b5f5bac
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2017-05-21 17:44:21 -0400

    added lock image for locker

--- i3/.config/i3/i3lock/locker.sh
+++ i3/.config/i3/i3lock/locker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-scrot /tmp/screen_locked.png -c -d 1
-convert /tmp/screen_locked.png -scale 2.5% -scale 4000% /tmp/screen_locked2.png
-i3lock -f -i /tmp/screen_locked2.png
+#scrot /tmp/screen_locked.png -c -d 1
+#convert /tmp/screen_locked.png -scale 2.5% -scale 5000% /tmp/screen_locked2.png
+i3lock -f -i /home/ryan/Desktop/lock.png
