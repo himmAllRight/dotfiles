9b64a99a87ff88395dd286a2341903e83aac74b7
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2017-11-21 15:30:52 -0500

    xrandr command for 2 work monitors

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,6 +1,9 @@
 #xrandr --auto --output LVDS-0 --rotate normal --mode 1920x1080 
-xrandr --auto --output DP-2 --rotate right --mode 1920x1080 --left-of HDMI-0 --output HDMI-0 --rotate left --mode 1920x1080
+#xrandr --auto --output DP-2 --rotate right --mode 1920x1080 --left-of HDMI-0 --output HDMI-0 --rotate left --mode 1920x1080
 #xrandr --auto --output DVI-I-3 --rotate normal --mode 1920x1080 --left-of DVI-I-2 --output DVI-I-2 --rotate right --mode 1920x1080
 
 
 #nitrogen --restore
+
+# Work Monitors?
+xrandr --auto --output VGA-1 --mode 1920x1080 --right-of HDMI-3 --output HDMI-3 --mode 1920x1080
