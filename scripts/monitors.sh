4808b2bfe92583ce857b1950e05396fcf2279d88
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2017-12-06 11:15:28 -0500

    Setup monitor script for home docking station

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #xrandr --auto --output LVDS-0 --rotate normal --mode 1920x1080 
 #xrandr --auto --output DP-2 --rotate right --mode 1920x1080 --left-of HDMI-0 --output HDMI-0 --rotate left --mode 1920x1080
 #xrandr --auto --output DVI-I-3 --rotate normal --mode 1920x1080 --left-of DVI-I-2 --output DVI-I-2 --rotate right --mode 1920x1080
@@ -5,5 +7,5 @@
 
 #nitrogen --restore
 
-# Work Monitors?
-xrandr --auto --output VGA-1 --mode 1920x1080 --right-of HDMI-3 --output HDMI-3 --mode 1920x1080
+# Home Dock Monitors?
+xrandr --auto --output DP-2-1 --mode 1920x1080 --right-of DP-2-2 --output DP-2-2 --mode 1920x1080 --dpi 90
