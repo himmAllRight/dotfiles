23dd07210bfa4c1d6399e9709fa8ec89fdd01cb3
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2017-12-07 07:41:12 -0500

    Upgraded the monitors script by organizing into functions it so I can expand it.

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-#xrandr --auto --output LVDS-0 --rotate normal --mode 1920x1080 
-#xrandr --auto --output DP-2 --rotate right --mode 1920x1080 --left-of HDMI-0 --output HDMI-0 --rotate left --mode 1920x1080
-#xrandr --auto --output DVI-I-3 --rotate normal --mode 1920x1080 --left-of DVI-I-2 --output DVI-I-2 --rotate right --mode 1920x1080
-
-
-#nitrogen --restore
+usage() {
+	  echo -e "monitors.sh\n\tThis script sets up my xrandr monitor configurations. Helpful when using i3.\n"
+	  echo "Usage:"
+	  echo "  undock                          - Returns monitor configuration to standard internal display only"
+	  echo "  home-dock                       - Configures laptop to use the monitors connected to my home workstation dock"
+}
 
 # Home Dock Monitors?
-xrandr --auto --output DP-2-1 --mode 1920x1080 --right-of DP-2-2 --output DP-2-2 --mode 1920x1080 --dpi 90
+home-dock() {
+    xrandr --auto --output DP-2-1 --mode 1920x1080 --right-of DP-2-2 --output DP-2-2 --mode 1920x1080 --dpi 90
+}
+
+# Use just standard internal laptop screen
+standard() {
+    echo "TODO"
+    ## TODO
+    ## xrandr --auto --output DP-2-1 --mode 1920x1080 --right-of DP-2-2 --output DP-2-2 --mode 1920x1080 --dpi 90
+}
+
+
+main() {
+	  local cmd=$1
+
+	  if [[ -z "$cmd" ]]; then
+		    usage
+		    exit 1
+	  fi
+
+	  if [[ $cmd == "undock" ]]; then
+		    standard
+    elif [[ $cmd == "home-dock" ]]; then
+        home-dock
+	  else
+		    usage
+	  fi
+}
+
+main "$@"
