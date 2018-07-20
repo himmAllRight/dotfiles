6c4ebf87f8c01b076a4019d88e0b81c391f57781
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2018-07-20 08:33:23 -0400

    Got it to where it was before, but with objects now.

--- emacs/.emacs-scripts/scrum-fns.el
+++ emacs/.emacs-scripts/scrum-fns.el
@@ -1,5 +1,13 @@
 ;; Writting some org functions to help me manage my stuff easier.
 
+(defclass ryan/task ()
+  ((level    :initarg :level    :initform 0)
+   (todo     :initarg :todo     :initform nil)
+   (priority :initarg :priority :initform nil)
+   (title    :initarg :title    :initform "")
+   (tags     :initarg :tags     :initform nil)
+   (time     :initarg :time     :initform 0)))
+
 (defun ryan/generate-task-table ()
   "Generates a table from a sub tree"
   (interactive)
@@ -11,16 +19,14 @@
       (open-line 1))
 
     (apply #'insert
-	   '("| Task | Time (hr) |\n"
-	     "|----|\n"))
+	   '("\n| Task | Time (hr) |"
+	     "\n|----|"))
     
     (apply #'insert output-list)
 
     (org-cycle)
 
     ))
-
-(setf foo nil)
 
 (defun ryan/get-org-task-list-data ()
   "Parses a sub tree and grabs the data to generate the table from."
@@ -39,37 +45,10 @@
 			   :title    title
 			   :tags     tags
 			   :time     (when time (string-to-number time)))
-	    foo))))
+	    output-items))))
      nil
      'tree)
     output-items))
-
-;; (defun ryan/get-org-task-list-data ()
-;;   "Parses a sub tree and grabs the data to generate the table from."
-;;   (interactive)
-;;   (let (output-items)
-;;     (org-map-entries
-;;      (lambda ()
-;;        (let* ((item-data (org-heading-components))
-;; 	      (level    (elt item-data 0))
-;; 	      (todo     (elt item-data 2))
-;; 	      (priority (elt item-data 3))
-;; 	      (title    (elt item-data 4))
-;; 	      (tags     (elt item-data 5))
-;; 	      (time     (org-element-property :TIME (org-element-at-point))))
-;; 	 (push
-;; 	  (make-instance 'ryan/task
-;; 			 :level    level
-;; 			 :todo     todo
-;; 			 :priority priority
-;; 			 :title    title
-;; 			 :tags     tags
-;; 			 :time     time)
-;; 	  foo)))
-;;      nil
-;;      'tree)
-;;     output-items))
-
 
 (defun calc-table-output (parsed-list)
   "Takes the parsed list of lists for the table, sums up the total
@@ -79,44 +58,33 @@
 	(output-list))
     (reduce (lambda (prev-values obj)
 	      (let ((prev-level   (car prev-values))
-		    (time        (if (cadr header-list)
-				     (string-to-number
-				      (cadr header-list))
+		    (time        (if (oref obj :time)
+				     (oref obj :time)
 				   0)))
-		(cond ((= prev-level (car header-data))
+		;; TODO: Cleanup all this redundancy
+		(cond ((= prev-level (oref obj :level))
 		       (push (format "\n| %s | %s |"
-				     (elt header-data 4)
+				     (oref obj :title)
 				     time)
 			     output-list)
-		       (cons (car header-data) (+ (cdr prev-values) time)))
-		      ((<= prev-level (car header-data))
+		       (cons (oref obj :level) (+ (cdr prev-values) time)))
+		      ((<= prev-level (oref obj :level))
 		       (push (format "\n| %s | %s |"
-				     (elt header-data 4)
+				     (oref obj :title)
 				     time)
 			     output-list)
-		       (cons (car header-data) 0))
-		      ((> prev-level (car header-data))
+		       (cons (oref obj :level) 0))
+		      ((> prev-level (oref obj :level))
 		       (push (format "\n| %s | %s |"
-				     (elt header-data 4)
+				     (oref obj :title)
 				     (+ time (cdr prev-values)))
 			     output-list)
-		       (cons (car header-data) (+ (cdr prev-values) time))))))
+		       (cons (oref obj :level) (+ (cdr prev-values) time))))))
 	    parsed-list :initial-value (cons 0 0))
     output-list))
 
-(org-element)
-(mapcar (lambda (item) (insert (format "\n%s" item))) (reverse foo))
 
-
-(defclass ryan/task ()
-  ((level    :initarg :level    :initform 0)
-   (todo     :initarg :todo     :initform nil)
-   (priority :initarg :priority :initform nil)
-   (title    :initarg :title    :initform "")
-   (tags     :initarg :tags     :initform nil)
-   (time     :initarg :time     :initform 0)))
-
-
+;; Scratch
 (setf foo nil)
 (setf bar (car foo))
 
